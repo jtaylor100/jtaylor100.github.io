705c186b3ccd915f3e1a63fe8b15a8f33a08391a
Josh Taylor <josh.taylor0686@gmail.com>
2018-09-20 20:42:30 +0200

    Added deployment script

---
+++ deploy.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout hakyll 
+
+# Build new files
+stack exec joshtaylorblog clean
+stack exec joshtaylorblog build
+
+# Get previous files
+git fetch --all
+git checkout master
+
+# Overwrite existing files with new files
+cp -a _site/. .
+
+# Commit
+git add -A
+git commit -m "Publish."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout hakyll 
